cb9c2761b5a670beee2ac54ec1bd80647bdf704f
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-28 10:35:56 +0100

    initial commit

---
+++ src/components/Movie.js
@@ -0,0 +1,28 @@
+import data from '../data'
+import './Movie.css'
+
+const Movie = ()=>{
+  return(
+  <div>
+     {
+     
+     data.map( (oneMovie) => {
+      const {id, image, title, age, tags, description} = oneMovie
+    
+      return (
+        <div key={id}>
+          <img src={image} alt="" />
+          <h2>{title}</h2>
+          <p>{age}</p>
+          <p>{tags}</p>
+          <p>{description}</p>
+        </div>
+      );
+     })
+    }
+
+  </div>
+  )
+}
+
+export default Movie
